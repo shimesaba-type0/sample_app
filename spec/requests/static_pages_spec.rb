aee77170cdf65d94c67df28e8fff9a6351964902
shimesaba-type0 <t.k.shimesaba@gmail.com>
2014-07-24 05:37:26 +0900

    Create a Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,28 +1,19 @@
-# require 'rails_helper'
-# 
-# RSpec.describe "StaticPages", :type => :request do
-#   describe "GET /static_pages" do
-#     it "works! (now write some real specs)" do
-#       get static_pages_index_path
-#       expect(response.status).to be(200)
-#     end
-#   end
-# end
-
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content("Sample App")
-      # page.should have_content "Sample App"
     end
 
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -34,7 +25,7 @@ describe "Static pages" do
 
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -46,7 +37,19 @@ describe "Static pages" do
 
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the contact 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
